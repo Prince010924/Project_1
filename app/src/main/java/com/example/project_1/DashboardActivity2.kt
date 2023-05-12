dd9275d2ea3566d3741c0e6a10a118a41d975590
prince010924 <neosenoamadi84@gmail.com>
2023-05-12 12:18:06 +0200

    added routing to dashboard after login

--- app/src/main/java/com/example/project_1/DashboardActivity2.kt
+++ app/src/main/java/com/example/project_1/DashboardActivity2.kt
@@ -1,11 +1,22 @@
 package com.example.project_1
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+private lateinit var welcomeText: String
+private lateinit var tvWelcome: TextView
+
+
 
 class DashboardActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard2)
+
+
+
+        welcomeText = "Welcome " + intent.getStringExtra("username") + "!";
+        tvWelcome = findViewById(R.id.tvWelcome);
+        tvWelcome.setText(welcomeText);
     }
 }
